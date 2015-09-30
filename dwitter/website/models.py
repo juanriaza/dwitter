4fc16ab0157e9e5f81b693c7193da61e4918c844
Juan Riaza <github@juanriaza.com>
2015-10-01 00:25:04 +0200

    extend user model

--- dwitter/website/models.py
+++ dwitter/website/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    bio = models.CharField(blank=True, max_length=200)
 
 
 class Tweet(models.Model):
     message = models.TextField(blank=True, max_length=140)
     timestamp = models.DateTimeField(default=timezone.now)
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(CustomUser)
 
     def __str__(self):
         return self.message
