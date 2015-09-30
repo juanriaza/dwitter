51289275869d027e7d70487d6747604f0434ce53
Juan Riaza <github@juanriaza.com>
2015-09-30 20:04:36 +0200

    Create first template

--- dwitter/website/views.py
+++ dwitter/website/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from django.utils import timezone
+from django.shortcuts import render
+
+
+tweets = [
+    {'user_name': 'jorgebastida', 'message': 'Hello world!', 'timestamp': timezone.now()},
+    {'user_name': 'jaimeirurzun', 'message': 'I like ponies! http://djangopony.com', 'timestamp': timezone.now()},
+    {'user_name': 'jorgebastida', 'message': 'Django RULEZZZZZZ', 'timestamp': timezone.now()}
+]
 
 
 def timeline(request):
-    return HttpResponse('Welcome to dwitter!')
+    return render(request, 'website/index.html', {'tweets': tweets})
